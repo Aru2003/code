8b7589dcc54fde4645bbc413d13f5d168b586942
Aru2003 <baymolda03@inbox.ru>
2023-02-07 08:41:03 +0600

    квадраттық функция құрдық

--- demo.java
+++ demo.java
@@ -1,3 +1,10 @@
 public static void main(String[] args){
     System.out.println("Hello World");
+    int a=12;
+    int res;
+    res= kvadrat(a);
+    System.out.print(res);
+}
+public static int kvadrat(int a){
+    return a*a;
 }
