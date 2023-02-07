c0348faea621c25fd110b7323ec72e55f474bad8
Aru2003 <baymolda03@inbox.ru>
2023-02-07 08:30:20 +0600

    if elif else шартты операторларын қолдандып ,коод жаздық

--- python.py
+++ python.py
@@ -1,2 +1,9 @@
 print("Baymolda Aruzhan")
 print("3-курс")
+age = int(input("Input:"))
+if(age >18):
+    print("adult")
+elif(age<18):
+    print("teenager or child ")
+else:
+    print("age is:",age)
